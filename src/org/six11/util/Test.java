3ab0b0bd6575cbb1458108aeacc003653c3178bf
gabe.johnson <gabe.johnson@ccb4a462-cb08-11de-a774-41d3a1f294e1>
2009-11-07 00:11:35 +0000

    Adding util files.

---
+++ src/org/six11/util/Test.java
@@ -0,0 +1,29 @@
+package org.six11.util;
+
+import org.six11.util.adt.*;
+import org.six11.util.adt.Graph.*;
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ **/
+public class Test {
+
+  private String[] args;
+
+  public static void main(String[] args) {
+    new Test(args).go();
+  }
+
+  public Test(String[] args) {
+    this.args = args;
+  }
+
+  public void go() {
+
+  }
+
+}
