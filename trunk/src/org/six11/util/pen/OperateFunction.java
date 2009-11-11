79e16088e3ee8b3bdf27a6ec26e239c0f7d9e363
gabe.johnson <gabe.johnson@ccb4a462-cb08-11de-a774-41d3a1f294e1>
2009-11-11 20:30:54 +0000

    wisdom

--- trunk/src/org/six11/util/pen/OperateFunction.java
+++ trunk/src/org/six11/util/pen/OperateFunction.java
@@ -7,3 +7,8 @@ public interface OperateFunction {
   public void operate(Sequence seq, Pt penLocation);
   public void endOperation();
 }
+
+// We did not invent the algorithm. The algorithm consistently finds
+// Jesus. The algorithm killed Jeeves. The algorithm is banned in
+// China. The algorithm is from Jersey. The algorithm constantly finds
+// Jesus. This is not the algorithm. This is close.
