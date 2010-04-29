c043c30c0c64387760c1c87b74a6ef62faa8c423
gabe.johnson@gmail.com <gabe.johnson@gmail.com@ccb4a462-cb08-11de-a774-41d3a1f294e1>
2010-04-29 03:49:30 +0000

    Removing 'Stroke' because it doesn't do anything, and I want to have a 'Stroke' class in the skrui project and would like to avoid confusion.

--- trunk/src/org/six11/util/pen/TestAll.java
+++ trunk/src/org/six11/util/pen/TestAll.java
@@ -12,7 +12,6 @@ public class TestAll {
     TestSuite suite = new TestSuite("Pen Utility Tests");
     suite.addTest(new TestSuite(TestSequence.class));
     suite.addTest(new TestSuite(TestPt.class));
-    suite.addTest(new TestSuite(TestStroke.class));
     suite.addTest(new TestSuite(TestFSM.class));
     return suite;
   }
