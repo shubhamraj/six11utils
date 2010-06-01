93bb3f9e04e9eadaf4bf9f4bcecfabb8c918c786
gabe.johnson <gabe.johnson@ccb4a462-cb08-11de-a774-41d3a1f294e1>
2010-06-01 22:43:48 +0000

    Made the main function a little easier to read, and produces more output.

--- src/org/six11/util/math/PCA.java
+++ src/org/six11/util/math/PCA.java
@@ -23,37 +23,145 @@ import org.six11.util.Debug;
  */
 public class PCA {
 
+  /**
+   * Run an example of PCA using data from disk, or canned data. If no file is specified, the canned
+   * data is hard-coded, and is the same as the 10-sample data from Lindsay Smith's fantastic PCA
+   * tutorial:
+   * 
+   * <pre>
+   * 2.5 2.4
+   * 0.5 0.7
+   * 2.2 2.9
+   * 1.9 2.2
+   * 3.1 3.0
+   * 2.3 2.7
+   * 2 1.6
+   * 1 1.1
+   * 1.5 1.6
+   * 1.1 0.9
+   * </pre>
+   * 
+   * @param args
+   * @throws IOException
+   */
   public static void main(String[] args) throws IOException {
-    File inFile = new File(args[0]);
-    List<List<Double>> data = new ArrayList<List<Double>>();
-    BufferedReader br = new BufferedReader(new FileReader(inFile));
-    int numElements = -1;
-    while (br.ready()) {
-      StringTokenizer line = new StringTokenizer(br.readLine());
-      List<Double> vec = new ArrayList<Double>();
-      data.add(vec);
-      while (line.hasMoreTokens()) {
-        vec.add(Double.parseDouble(line.nextToken()));
+    double[][] matrix;
+    String source = null;
+    if (args.length > 0) {
+      source = "from disk";
+      List<List<Double>> data = new ArrayList<List<Double>>();
+      File inFile = new File(args[0]);
+      BufferedReader br = new BufferedReader(new FileReader(inFile));
+      int numElements = -1;
+      while (br.ready()) {
+        StringTokenizer line = new StringTokenizer(br.readLine());
+        List<Double> vec = new ArrayList<Double>();
+        data.add(vec);
+        while (line.hasMoreTokens()) {
+          vec.add(Double.parseDouble(line.nextToken()));
+        }
+        numElements = vec.size();
       }
-      numElements = vec.size();
-    }
-    double[][] matrix = new double[data.size()][numElements];
-    for (int i = 0; i < data.size(); i++) {
-      List<Double> vec = data.get(i);
-      for (int j = 0; j < vec.size(); j++) {
-        matrix[i][j] = vec.get(j);
+      matrix = new double[data.size()][numElements];
+      for (int i = 0; i < data.size(); i++) {
+        List<Double> vec = data.get(i);
+        for (int j = 0; j < vec.size(); j++) {
+          matrix[i][j] = vec.get(j);
+        }
       }
+    } else {
+      source = "from the Smith PCA tutorial";
+      matrix = new double[][] {
+          {
+              2.5, 2.4
+          }, {
+              0.5, 0.7
+          }, {
+              2.2, 2.9
+          }, {
+              1.9, 2.2
+          }, {
+              3.1, 3.0
+          }, {
+              2.3, 2.7
+          }, {
+              2, 1.6
+          }, {
+              1, 1.1
+          }, {
+              1.5, 1.6
+          }, {
+              1.1, 0.9
+          }
+      };
+      bug(" **");
+      bug(" ** Note: You can put data in a plain text file and mention its ");
+      bug(" ** name on the command line if you want to process different data.");
+      bug(" ** Just put each sample vector on a row, with scalar values whitespace-separated.");
+      bug(" ** Example of three samples with 2 dimensions:");
+      bug(" **");
+      bug(" ** 2.3 9.01");
+      bug(" ** 2.5 8.604");
+      bug(" ** 2.22 8.24");
+      bug(" **");
+      bug(" ** ALSO: note the eigenvalues in the Smith tutorial have different sign than the");
+      bug(" ** eigenvalues calculated here. This is not a problem, because the orientation should");
+      bug(" ** not matter as long as the orientations are used consistently.");
     }
+    Matrix originalData = new Matrix(matrix);
+    bug("Here is the original data from " + source + ", before adjusting by each dimension mean:");
+    originalData.print(8, 4);
+
     PCA pca = new PCA(matrix);
+
     int numComponents = pca.getNumComponents();
     bug("There are " + numComponents + " components");
-    int k = 2; 
+    int k = 2;
     List<PrincipleComponent> mainComponents = pca.getDominantComponents(k);
     int counter = 1;
     bug("Showing top " + k + " principle components.");
     for (PrincipleComponent pc : mainComponents) {
       bug("Component " + (counter++) + ": " + pc);
     }
+    Matrix features = PCA.getDominantComponentsMatrix(mainComponents);
+    bug("Feature matrix (k=" + k + ") :");
+    features.print(8, 4);
+
+    Matrix featuresXpose = features.transpose();
+    bug("Xposed feature matrix (k=" + k + ") :");
+    featuresXpose.print(8, 4);
+
+    double[][] matrixAdjusted = PCA.getMeanAdjusted(matrix, pca.getMeans());
+    Matrix adjustedInput = new Matrix(matrixAdjusted);
+    bug("Original input adjusted by dimension means (k=" + k + ") :");
+    adjustedInput.print(8, 4);
+    Matrix xformedData = featuresXpose.times(adjustedInput.transpose());
+    bug("Transformed data into PCA-space (k=" + k + ") :");
+    xformedData.transpose().print(8, 4);
+
+    k = 1;
+    mainComponents = pca.getDominantComponents(k);
+    counter = 1;
+    bug("Showing top " + k + " principle components.");
+    for (PrincipleComponent pc : mainComponents) {
+      bug("Component " + (counter++) + ": " + pc);
+    }
+    features = PCA.getDominantComponentsMatrix(mainComponents);
+    bug("Feature matrix (k=" + k + ") :");
+    features.print(8, 4);
+
+    featuresXpose = features.transpose();
+    bug("Xposed feature matrix (k=" + k + ") :");
+    featuresXpose.print(8, 4);
+
+    matrixAdjusted = PCA.getMeanAdjusted(matrix, pca.getMeans());
+    adjustedInput = new Matrix(matrixAdjusted);
+    bug("Original input adjusted by dimension means (k=" + k + ") :");
+    adjustedInput.print(8, 4);
+    xformedData = featuresXpose.times(adjustedInput.transpose());
+    bug("Transformed data into PCA-space (k=" + k + ") :");
+    xformedData.transpose().print(8, 4);
+
   }
 
   Matrix covMatrix;
@@ -61,16 +169,15 @@ public class PCA {
   double[] eigenvalues;
   Matrix eigenvectors;
   SortedSet<PrincipleComponent> principleComponents;
+  double[] means;
 
   public PCA(double[][] input) {
-    double[][] cov = getCovariance(input);
+    means = new double[input.length];
+    double[][] cov = getCovariance(input, means);
     covMatrix = new Matrix(cov);
     eigenstuff = covMatrix.eig();
     eigenvalues = eigenstuff.getRealEigenvalues();
-    bug("eigenvalues: " + Debug.num(eigenvalues));
     eigenvectors = eigenstuff.getV();
-    bug("eigenvector matrix:");
-    eigenvectors.print(6, 3);
     double[][] vecs = eigenvectors.getArray();
     int numComponents = eigenvectors.getColumnDimension(); // same as num rows.
     principleComponents = new TreeSet<PrincipleComponent>();
@@ -81,6 +188,32 @@ public class PCA {
       }
       principleComponents.add(new PrincipleComponent(eigenvalues[i], eigenvector));
     }
+  }
+
+  public double[] getMeans() {
+    return means;
+  }
+
+  /**
+   * Subtracts the mean value from each column. The means must be precomputed, which you get for
+   * free when you make a PCA instance (just call getMeans()).
+   * 
+   * @param input
+   *          Some data, where each row is a sample point, and each column is a dimension.
+   * @param mean
+   *          Subtract the dimension's mean from each value.
+   * @return
+   */
+  public static double[][] getMeanAdjusted(double[][] input, double[] mean) {
+    int nRows = input.length;
+    int nCols = input[0].length;
+    double[][] ret = new double[nRows][nCols];
+    for (int row = 0; row < nRows; row++) {
+      for (int col = 0; col < nCols; col++) {
+        ret[row][col] = input[row][col] - mean[col];
+      }
+    }
+    return ret;
   }
 
   public List<PrincipleComponent> getDominantComponents(int n) {
@@ -94,6 +227,18 @@ public class PCA {
       }
     }
     return ret;
+  }
+
+  public static Matrix getDominantComponentsMatrix(List<PrincipleComponent> dom) {
+    int nRows = dom.get(0).eigenVector.length;
+    int nCols = dom.size();
+    Matrix matrix = new Matrix(nRows, nCols);
+    for (int col = 0; col < nCols; col++) {
+      for (int row = 0; row < nRows; row++) {
+        matrix.set(row, col, dom.get(col).eigenVector[row]);
+      }
+    }
+    return matrix;
   }
 
   public int getNumComponents() {
@@ -125,7 +270,7 @@ public class PCA {
     }
   }
 
-  public static double[][] getCovariance(double[][] input) {
+  public static double[][] getCovariance(double[][] input, double[] meanValues) {
     int numDataVectors = input.length;
     int n = input[0].length;
 
@@ -147,6 +292,9 @@ public class PCA {
         ret[i][j] = v;
         ret[j][i] = v;
       }
+    }
+    if (meanValues != null) {
+      System.arraycopy(mean, 0, meanValues, 0, mean.length);
     }
     return ret;
   }
