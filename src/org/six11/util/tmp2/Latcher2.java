d1e6aa9f910ece40be478f3ba865aa5c75bbbc33
gabe.johnson <gabe.johnson@gmail.com>
2011-05-03 16:14:19 +0000

    switching comps

---
+++ src/org/six11/util/tmp2/Latcher2.java
@@ -0,0 +1,17 @@
+package org.six11.util.tmp2;
+
+import org.six11.util.Debug;
+
+public class Latcher2 {
+  
+  private JunctionFinder jf;
+
+  public Latcher2(JunctionFinder jf) {
+    this.jf = jf;
+  }
+
+  public void latch() {
+
+  }
+  
+}
