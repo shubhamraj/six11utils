a716e24976007d29114d352dabebb96e97e50274
gabe.johnson <gabe.johnson@gmail.com>
2011-08-30 17:16:48 +0000

    A simple test file for trying things out

---
+++ src/org/six11/util/Test.java
@@ -0,0 +1,21 @@
+package org.six11.util;
+
+import static org.six11.util.Debug.bug;
+
+/**
+ * 
+ *
+ * @author Gabe Johnson <johnsogg@cmu.edu>
+ */
+public class Test {
+
+  public static void main(String[] args) {
+    bug("Test.main not implemented yet!");
+    reallyLongFunctionName();
+  }
+
+  private static void reallyLongFunctionName() {
+    bug("abcdefghijklmnopqrstuvwxyz");
+  }
+
+}
