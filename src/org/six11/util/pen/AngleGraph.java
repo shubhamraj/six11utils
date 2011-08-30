190b07073ae02f250edefccfd301fbd0b688875b
gabe.johnson <gabe.johnson@gmail.com>
2011-08-30 17:15:29 +0000

    Several changes related to the thesis-code project. Got rid of the 'tmp' stuff, which never should have been there to start with....

--- src/org/six11/util/pen/AngleGraph.java
+++ src/org/six11/util/pen/AngleGraph.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.six11.util.Debug;
-import org.six11.util.tmp.AntSegment;
+//import org.six11.util.tmp.AntSegment;
 
 /**
  * A datastructure that stores line segments ordered by their angle.
@@ -38,126 +38,126 @@ import org.six11.util.tmp.AntSegment;
  * @author Gabe Johnson <johnsogg@cmu.edu>
  */
 public class AngleGraph {
-
-  public static void main(String[] args) {
-
-  }
-
-  List<AntSegment> segments;
-  static Comparator<AntSegment> orderByAngle = new Comparator<AntSegment>() {
-
-    public int compare(AntSegment o1, AntSegment o2) {
-      return ((Double) o1.getFixedAngle()).compareTo(o2.getFixedAngle());
-    }
-
-  };
-
-  public AngleGraph() {
-    segments = new ArrayList<AntSegment>();
-  }
-
-  public void add(AntSegment seg) {
-    int where = Collections.binarySearch(segments, seg, orderByAngle);
-    if (where < 0) {
-      where = (where + 1) * -1;
-    }
-    segments.add(where, seg);
-  }
-
-  public void remove(AntSegment p) {
-    segments.remove(p);
-  }
-
-  public Set<AntSegment> getNear(AntSegment in, double angle, double tolerance) {
-    Set<AntSegment> buddies = getNear(in.getFixedAngle() + angle, tolerance);
-    buddies.remove(in);
-    return buddies;
-  }
-
-  /**
-   * Gives a set of line segments that are about the given angle, plus or minus the tolerance. For
-   * example, say your input segment is almost horizontal (angle is about 0.1). If you are
-   * interested in parallel lines, supply angle of 0.1. If you want perpendicular lines, use angle
-   * of 0.1 + Pi/2.
-   * 
-   * Then, supply a tolerance that describes how much error you're willing to deal with. A pretty
-   * tight tolerance is 12 degrees (0.21 radians); a loose tolerance is 30 degrees (0.52 radians).
-   * These are the values Alvarado used in her thesis. The returned segments have angles that are
-   * +/- tolerance/2 of the angle.
-   * 
-   * Both angles are interpreted as radians.
-   * 
-   * This returns the set of AntSegments that are within tolerance radians of the given angle.
-   */
-  public Set<AntSegment> getNear(double angle, double tolerance) {
-    double half = tolerance / 2;
-    Set<AntSegment> ret = new HashSet<AntSegment>();
-    double theta = getNormalizedAngle(angle);
-    double a = getNormalizedAngle(theta - half);
-    double b = getNormalizedAngle(theta + half);
-    if (a < theta && theta <= b) {
-      ret.addAll(search(a, b));
-    } else {
-      ret.addAll(search(-Math.PI / 2, b));
-      ret.addAll(search(a, Math.PI / 2));
-    }
-    return ret;
-  }
-
-  private Set<AntSegment> search(double a, double b) {
-    Set<AntSegment> ret = new HashSet<AntSegment>();
-    int idxA = search(a);
-    int idxB = search(b);
-    for (int cursor = idxA; cursor <= idxB; cursor++) {
-      ret.add(segments.get(cursor));
-    }
-    return ret;
-  }
-
-  /**
-   * Returns the index where a line segment with the given angle would be inserted.
-   */
-  private int search(double target) {
-    return search(0, segments.size(), target);
-  }
-
-  private int search(int low, int high, double target) {
-    int mid = (low + high) / 2;
-    double thisAngle = segments.get(mid).getFixedAngle();
-    if (thisAngle == target) {
-      return mid;
-    } else {
-      // when the search comes down to adjacent slots, the insert point is going to be before or
-      // between the slots.
-      if (high - low == 1) {
-        if (segments.get(low).getFixedAngle() > target) {
-          return low;
-        } else {
-          return Math.min(high, segments.size() - 1);
-        }
-      }
-      if (target < thisAngle) {
-        return search(low, mid, target);
-      } else {
-        return search(mid, high, target);
-      }
-    }
-  }
-
-  private double getNormalizedAngle(double angle) {
-    double ret = angle;
-    while (ret > Math.PI / 2) {
-      ret = ret - Math.PI;
-    }
-    while (ret < -Math.PI / 2) {
-      ret = ret + Math.PI;
-    }
-    return ret;
-  }
-
-  @SuppressWarnings("unused")
-  private static void bug(String what) {
-    Debug.out("AngleGraph", what);
-  }
+//
+//  public static void main(String[] args) {
+//
+//  }
+//
+//  List<AntSegment> segments;
+//  static Comparator<AntSegment> orderByAngle = new Comparator<AntSegment>() {
+//
+//    public int compare(AntSegment o1, AntSegment o2) {
+//      return ((Double) o1.getFixedAngle()).compareTo(o2.getFixedAngle());
+//    }
+//
+//  };
+//
+//  public AngleGraph() {
+//    segments = new ArrayList<AntSegment>();
+//  }
+//
+//  public void add(AntSegment seg) {
+//    int where = Collections.binarySearch(segments, seg, orderByAngle);
+//    if (where < 0) {
+//      where = (where + 1) * -1;
+//    }
+//    segments.add(where, seg);
+//  }
+//
+//  public void remove(AntSegment p) {
+//    segments.remove(p);
+//  }
+//
+//  public Set<AntSegment> getNear(AntSegment in, double angle, double tolerance) {
+//    Set<AntSegment> buddies = getNear(in.getFixedAngle() + angle, tolerance);
+//    buddies.remove(in);
+//    return buddies;
+//  }
+//
+//  /**
+//   * Gives a set of line segments that are about the given angle, plus or minus the tolerance. For
+//   * example, say your input segment is almost horizontal (angle is about 0.1). If you are
+//   * interested in parallel lines, supply angle of 0.1. If you want perpendicular lines, use angle
+//   * of 0.1 + Pi/2.
+//   * 
+//   * Then, supply a tolerance that describes how much error you're willing to deal with. A pretty
+//   * tight tolerance is 12 degrees (0.21 radians); a loose tolerance is 30 degrees (0.52 radians).
+//   * These are the values Alvarado used in her thesis. The returned segments have angles that are
+//   * +/- tolerance/2 of the angle.
+//   * 
+//   * Both angles are interpreted as radians.
+//   * 
+//   * This returns the set of AntSegments that are within tolerance radians of the given angle.
+//   */
+//  public Set<AntSegment> getNear(double angle, double tolerance) {
+//    double half = tolerance / 2;
+//    Set<AntSegment> ret = new HashSet<AntSegment>();
+//    double theta = getNormalizedAngle(angle);
+//    double a = getNormalizedAngle(theta - half);
+//    double b = getNormalizedAngle(theta + half);
+//    if (a < theta && theta <= b) {
+//      ret.addAll(search(a, b));
+//    } else {
+//      ret.addAll(search(-Math.PI / 2, b));
+//      ret.addAll(search(a, Math.PI / 2));
+//    }
+//    return ret;
+//  }
+//
+//  private Set<AntSegment> search(double a, double b) {
+//    Set<AntSegment> ret = new HashSet<AntSegment>();
+//    int idxA = search(a);
+//    int idxB = search(b);
+//    for (int cursor = idxA; cursor <= idxB; cursor++) {
+//      ret.add(segments.get(cursor));
+//    }
+//    return ret;
+//  }
+//
+//  /**
+//   * Returns the index where a line segment with the given angle would be inserted.
+//   */
+//  private int search(double target) {
+//    return search(0, segments.size(), target);
+//  }
+//
+//  private int search(int low, int high, double target) {
+//    int mid = (low + high) / 2;
+//    double thisAngle = segments.get(mid).getFixedAngle();
+//    if (thisAngle == target) {
+//      return mid;
+//    } else {
+//      // when the search comes down to adjacent slots, the insert point is going to be before or
+//      // between the slots.
+//      if (high - low == 1) {
+//        if (segments.get(low).getFixedAngle() > target) {
+//          return low;
+//        } else {
+//          return Math.min(high, segments.size() - 1);
+//        }
+//      }
+//      if (target < thisAngle) {
+//        return search(low, mid, target);
+//      } else {
+//        return search(mid, high, target);
+//      }
+//    }
+//  }
+//
+//  private double getNormalizedAngle(double angle) {
+//    double ret = angle;
+//    while (ret > Math.PI / 2) {
+//      ret = ret - Math.PI;
+//    }
+//    while (ret < -Math.PI / 2) {
+//      ret = ret + Math.PI;
+//    }
+//    return ret;
+//  }
+//
+//  @SuppressWarnings("unused")
+//  private static void bug(String what) {
+//    Debug.out("AngleGraph", what);
+//  }
 
 }
