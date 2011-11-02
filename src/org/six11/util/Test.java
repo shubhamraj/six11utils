486c9acced030c21f147834ff73239e114612f85
gabe.johnson <gabe.johnson@gmail.com>
2011-11-02 22:24:03 +0000

    The sketcher will need a way to cluster things together (lengths of line segments, angles, etc) so I am adapting the Dendogram from the PCA/handwriting thing to use in the sketcher (in a more generic way)

--- src/org/six11/util/Test.java
+++ src/org/six11/util/Test.java
@@ -1,21 +1,43 @@
 package org.six11.util;
 
 import static org.six11.util.Debug.bug;
+import static org.six11.util.Debug.num;
+
+import java.util.List;
+
+import org.six11.util.math.ClusterThing;
+import org.six11.util.math.ClusterThing.Cluster;
 
 /**
  * 
- *
+ * 
  * @author Gabe Johnson <johnsogg@cmu.edu>
  */
 public class Test {
 
   public static void main(String[] args) {
-    bug("Test.main not implemented yet!");
-    reallyLongFunctionName();
-  }
-
-  private static void reallyLongFunctionName() {
-    bug("abcdefghijklmnopqrstuvwxyz");
+    Debug.useColor = false;
+    ClusterThing<String> clusters = new ClusterThing<String>() {
+      public double query(String thing) {
+        double ret = 0;
+        try {
+          ret = Double.parseDouble(thing);
+        } catch (NumberFormatException ex) {
+          ret = 0;
+        }
+        return ret;
+      }
+    };
+    for (String v : args) {
+      bug(v);
+      clusters.add(v);
+    }
+    clusters.computeClusters();
+    double radius = clusters.getRadius();
+    bug("Radius of whole thing: " + num(radius));
+    for (String v : args) {
+//      List<Cluster> x = clusters.search(v);
+    }
   }
 
 }
