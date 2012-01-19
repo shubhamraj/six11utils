f37f4c3825ea9ffeb0f7957da5edcb1d376512e5
gabe.johnson <gabe.johnson@gmail.com>
2012-01-19 18:53:40 +0000

    debugging something

--- src/org/six11/util/Test.java
+++ src/org/six11/util/Test.java
@@ -22,16 +22,11 @@ import org.six11.util.pen.Vec;
 public class Test {
 
   public static void main(String[] args) {
-    Debug.useColor = false;
-    Pt pt = new Pt(50.0, 60.0);
-    bug("Point is " + num(pt) + " with hash code: " + pt.hashCode());
-    GaussianHat hat = new GaussianHat(5, 3);
-    for (int i = 0; i < 10; i++) {
-      bug("Point is " + num(pt) + " with hash code: " + pt.hashCode());
-      double randX = hat.getDouble();
-      double randY = hat.getDouble();
-      pt.move(new Vec(randX, randY));
-    }
+    Pt x = new Pt(1.234234234234234234234, Math.PI);
+    bug("Normal: " + num(x));
+    bug("Fancy 3: " + num(x, 3));
+    bug("Fancy 7: " + num(x, 7));
+    bug("Fancy 3: " + num(x, 3));
   }
 
 }
