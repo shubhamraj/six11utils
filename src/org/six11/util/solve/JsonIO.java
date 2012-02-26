07701632a1b4ef6749397e951e940c3dc94ca55b
gabe.johnson <gabe.johnson@gmail.com>
2012-02-26 21:24:40 +0000

    Starting to implement save in simi

--- src/org/six11/util/solve/JsonIO.java
+++ src/org/six11/util/solve/JsonIO.java
@@ -12,37 +12,6 @@ import org.json.*;
 
 public class JsonIO {
 
-  //  public static void main(String[] args) throws JSONException {
-  //    Debug.useColor = false;
-  //    JsonIO io = new JsonIO();
-  //
-  //    Pt a = new Pt(45.3934, 843.23, System.currentTimeMillis() - 238923);
-  //    a.setString("name", "point A");
-  //    a.setBoolean("pinned", false);
-  //
-  //    Pt b = new Pt(45.3934, 843.23, System.currentTimeMillis());
-  //    b.setString("name", "point B");
-  //    b.setBoolean("pinned", true);
-  //    b.setDouble("ignoreMe", 734.39);
-  //
-  //    JSONObject aObj = io.write(a, "name", "pinned");
-  //    bug("aObj:");
-  //    bug("" + aObj);
-  //    Pt aR = io.readPt(aObj, "pinned", "name");
-  //    
-  //    bug("Reconstructed: " + num(aR) + " named " + aR.getString("name") + " and pinned: " + aR.getBoolean("pinned"));
-  //    
-  //    List<Pt> ab = new ArrayList<Pt>();
-  //    ab.add(a);
-  //    ab.add(b);
-  //
-  //    JSONArray pointArray = io.write(ab, "name", "pinned");
-  //    List<Pt> reconst = io.readPoints(pointArray, "pinned", "name");
-  //    for (Pt p : reconst) {
-  //      bug(num(p) + " name: " + p.getString("name") + ", pinned: " + p.getBoolean("pinned"));
-  //    }
-  //  }
-
   public JSONObject write(Pt pt, String... persistedData) throws JSONException {
     JSONObject ret = new JSONObject();
     ret.put("x", pt.getX());
